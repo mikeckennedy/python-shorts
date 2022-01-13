0a3f2bf1d3da2e8cecba9a1e4e971fb2ae07252b
Michael Kennedy <mikeckennedy@gmail.com>
2022-01-13 11:41:39 -0800

    Final loop / generator short code

--- shorts/04-loop-to-comprehension-to-generator/loop-to-comprehension-to-generator.py
+++ shorts/04-loop-to-comprehension-to-generator/loop-to-comprehension-to-generator.py
@@ -1,10 +1,11 @@
 import json
 from pathlib import Path
-from typing import Iterable
+from typing import Iterator
 
 
 def main():
     sales = load_list()
+    #  print(sales)
 
     count = 10
     for idx, s in enumerate(sales, start=1):
@@ -26,7 +27,7 @@ class AutoCustomer:
     price: int
 
     def __init__(self, _id: int, first_name, last_name, email, make, model, year, vin, price):
-        # print(f"Creating customer: {first_name} {last_name}")
+        print(f"Creating customer: {first_name} {last_name}")
         self.id = _id
         self.first_name = first_name
         self.last_name = last_name
@@ -38,24 +39,28 @@ class AutoCustomer:
         self.price = price
 
 
-def load_list() -> Iterable[AutoCustomer]:
+def load_list() -> Iterator[AutoCustomer]:
     file = Path(__file__).parent / 'MOCK_DATA.json'
     with file.open('r') as fin:
         # Use json-stream package to stream this too.
         # We're just keeping it simple
         raw_data = json.load(fin)
 
-    customers = []
-    for entry in raw_data:
-        c = AutoCustomer(**entry)
-        customers.append(c)
+    # for-in loop
+
+    # customers = []
+    # for entry in raw_data:
+    #     c = AutoCustomer(**entry)
+    #     customers.append(c)
+
+    # List comprehension
+    # customers = [AutoCustomer(**entry) for entry in raw_data]
+
+    # Generator expression
+    customers = (AutoCustomer(**entry) for entry in raw_data)
 
     return customers
 
 
 if __name__ == '__main__':
     main()
-
-# htmx + fastapi + tailwind + mongodb
-# ACHN https://kivymd.readthedocs.io/en/latest/index.html -
-# https://www.youtube.com/channel/UCl72hvzwQ0rXUMBKTsKjRAA
