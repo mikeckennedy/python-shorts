ac67b2b2b84bc365dedf6b09b0c2837ab4c3cb59
Michael Kennedy <mikeckennedy@gmail.com>
2022-11-05 17:05:35 -0700

    Code for GC mods settings.

--- shorts/09-gc-settings/run_gc_configured.py
+++ shorts/09-gc-settings/run_gc_configured.py
@@ -17,11 +17,8 @@ def main(change_gc: bool):
     m0 = report_process_mem()
 
     if change_gc:
-        # g0, g1, allocs = gc.get_threshold()
-        # gc.set_threshold(g0 * 5, g1 * 10, 100_000)
-
         allocs, gen1, gen2 = gc.get_threshold()
-        allocs = 50_000  # Start the GC sequence every 10K not 700 class allocations.
+        allocs = 50_000  # Start the GC sequence every 50K not 700 class allocations.
         gc.set_threshold(allocs, gen1 * 5, gen2 * 10)
 
     lists_of_items = []
@@ -30,7 +27,7 @@ def main(change_gc: bool):
 
     m1 = report_process_mem()
 
-    return change_gc, m1 - m0
+    return m1 - m0
 
 
 def report_process_mem() -> int:
@@ -62,13 +59,13 @@ if __name__ == '__main__':
     random.seed(12345)
     create_some_items(1)
 
-    change_gc = input("Use modified GC settings? [y]/n ") in {'', 'y'}
+    change = input("Use modified GC settings? [y]/n ") in {'', 'y'}
 
     t0 = datetime.datetime.now()
-    changed_gc, mem_delta = main(change_gc)
+    mem_delta = main(change)
 
     dt = datetime.datetime.now() - t0
-    msg = (Fore.LIGHTRED_EX + 'with') if changed_gc else (Fore.CYAN + 'without')
+    msg = (Fore.LIGHTRED_EX + 'with') if change else (Fore.CYAN + 'without')
     print(f"Done {msg + Fore.RESET} modifying the GC.")
     print(f'Memory used: {mem_delta:,.0f} MB')
     print(f'Time:{Fore.LIGHTYELLOW_EX} {dt.total_seconds() * 1000:,.1f} ms')
